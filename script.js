7f597690a4b502de942456250605e1235e739984
Valakzz <chucklord14@gmail.com>
2026-03-19 19:26:10 -0300

    retirado duplicação

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 async function buscar() {
   let url = "https://dragonball-api.com/api/characters?limit=58";
   let allCharacters = [];
@@ -72,78 +72,3 @@ async function buscar() {
   }
 
 
-
-=======
- async function buscar(){
-    let url = "https://dragonball-api.com/api/characters?limit=58";
-      
-    
-      let raceFilter = ""
-
-
-      let race = document.querySelector("#race").value;
-      let gender = document.getElementsByTagName("input");
-      let genderfilter = ""
-      console.log(gender)
-      if(gender[1].checked){
-        genderfilter = `&gender=`+ gender[1].value;
-        
-      }else if(gender[2].checked){
-        genderfilter = `&gender=`+ gender[2].value;
-      }
-
-      if(race != ""){
-        raceFilter = `&race=` + race;
-           
-    }
-    url = url + raceFilter +  genderfilter;
- 
-     ;
-      document.querySelector(".allUsers").innerHTML = "";
- let resposta = await fetch(url)
-     let dados= await resposta.json();
-      allCharacters = dados.items;
-    
-      if(dados.items){
-        allCharacters = dados.items
-      }else{
-        allCharacters = dados
-      }
-
-
-      for(let  character of allCharacters){
-        console.log(character.name);   
-      //  console.log(character.race);   
-     //   console.log(character.image);  
-      //  console.log(character.ki);   
-       // console.log('---------------------------')
-        let divUser = document.createElement('div');
-        divUser.classList.add('user');
-         divUser.innerHTML = `  
-           <div class="avatar">
-                <img src="${character.image}" alt="">
-            </div>
-            <div class="data">
-                <p>${character.name}</p>
-                <p>
-                    <span>${character.race} - ${character.gender}</span>
-                </p>
-                <p>
-                    <span>Base KI:</span>
-                    <span>${character.ki}</span>
-                </p>
-                <p>
-                    <span>Total KI:</span>
-                    <span>${character.maxKi}</span>
-                </p>
-
-                
-
-                
-            </div>
-         `
-         document.querySelector(".allUsers").appendChild(divUser);
-      }
-     
-}
->>>>>>> 544dc0f5d7915908af594ac36aba2fecf04b070e
